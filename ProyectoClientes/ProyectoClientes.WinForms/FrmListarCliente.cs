33f5261238bae8c07d200c7fb37bba951b80d128
micasessa <micasessa@yahoo.com>
2021-11-04 14:08:51 -0300

    Creacion Formularios

--- ProyectoClientes/ProyectoClientes.WinForms/FrmListarCliente.cs
+++ ProyectoClientes/ProyectoClientes.WinForms/FrmListarCliente.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ProyectoClientes.WinForms
 {
-    public partial class Form1 : Form
+    public partial class FrmListarCliente : Form
     {
-        public Form1()
+        public FrmListarCliente()
         {
             InitializeComponent();
         }
