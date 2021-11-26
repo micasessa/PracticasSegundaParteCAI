262d3aae650a4e8429a460db99a3f4eb9d86cdc7
micasessa <micasessa@yahoo.com>
2021-11-25 23:48:48 -0300

    Desarrollo codigo en Forms + clase Operador formulario

--- EjercicioTarjetaCredito/EjercicioTarjetaCredito.Entidades/OperadorFormulario.cs
+++ EjercicioTarjetaCredito/EjercicioTarjetaCredito.Entidades/OperadorFormulario.cs
@@ -5,9 +5,60 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace EjercicioTarjetaCredito.Entidades
-{
+{   
     public class OperadorFormulario
     {
-        //FALTA DESARROLLAR
+        private List<TarjetaCredito> _tarjetas;
+        private int _cantidadTarjetas;
+        private int _limitePromedio;
+
+        //constructor
+        public OperadorFormulario(List <TarjetaCredito> lst)
+        {
+            _tarjetas = lst;
+        }
+
+        //PROPIEDADES
+        public List<TarjetaCredito> TraerTarjetas
+        {
+            get{ return _tarjetas; }
+        }
+
+        //Hago que me traiga la cant de tarjetas, validando antes que NO sea null
+        public int Cantidad
+        {
+            get
+            {
+                if (_tarjetas != null)
+                {
+                    return _tarjetas.Count;
+                }
+                else return 0;
+            }
+        }
+
+        //Calcula el limite promedio de todas las tarjetas que vinieron por el constructor
+        public double LimitePromedio
+        {
+            get
+            {   //Hago la misma validac q cantidad
+                if (_tarjetas != null)
+                {   //Primero pongo en cero los valores
+                    double promedio = 0;
+                    double total = 0;
+
+                    //Busco por c/a tarj cred en la lista que traje, sacar el total del limite de compra
+                    foreach(TarjetaCredito tc in _tarjetas)
+                    {
+                        total = tc.LimiteCompra;
+                    }
+                    //saco el promedio:
+                    promedio = total / _tarjetas.Count;
+                    return promedio;
+
+                }
+                else return 0;
+            }
+        }
     }
 }
--- EjercicioTarjetaCredito/EjercicioTarjetaCredito.WinForms/FrmTarjetaCredito.cs
+++ EjercicioTarjetaCredito/EjercicioTarjetaCredito.WinForms/FrmTarjetaCredito.cs
@@ -17,22 +17,94 @@ namespace EjercicioTarjetaCredito.WinForms
 {
     public partial class FrmTarjetaCredito : Form
     {
+        private ClienteNegocio _servicioCliente;
+        private TarjetaCreditoNegocio _servicioTarjeta;
+        private List<TarjetaCredito> _listaTarjetas;
         public FrmTarjetaCredito()
         {
+            _servicioCliente = new ClienteNegocio();
+            _servicioTarjeta = new TarjetaCreditoNegocio();
+            _listaTarjetas = new List<TarjetaCredito>();
+
             InitializeComponent();
+        }
+
+        private void FrmTarjetaCredito_Load(object sender, EventArgs e)
+        {
+            Carga();
+            Limpiar();
+            Recargar();
         }
 
         private void _btnCalcular_Click(object sender, EventArgs e)
         {
+            try
+            {
 
+            } catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void _btnAlta_Click(object sender, EventArgs e)
         {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        
+
+        private void Carga()
+        {   //Como traer con clases ENUM:
+            _cmbTipoTarjeta.DataSource = Enum.GetValues(typeof(TipoTarjetaEnum));
+            _cmbPeriodoCierre.DataSource = Enum.GetValues(typeof(PeriodoEnum));
+
+            //Como traer clientes
+            _cmbCliente.DataSource = null;
+            _cmbCliente.DataSource = _servicioCliente.GetClientes();
+            _cmbCliente.DisplayMember = "Display Cliente";
+            _cmbCliente.ValueMember = "Id";
+        }
+
+        private void Limpiar()
+        {
+            _cmbTipoTarjeta.SelectedIndex = 0;
+            _cmbPeriodoCierre.SelectedIndex = 0;
+            _txtBxLimiteCompra.Clear();
+            _txtNumPlastico.Clear();
+            _txtCantPlasticoEmitidos.Clear();
+            _txtLimitePromedio.Clear();
 
         }
 
-        private void FrmTarjetaCredito_Load(object sender, EventArgs e)
+        private void Recargar()
+        {
+            RecargarLista();
+            RecargarCalculo();
+        }
+
+        private void RecargarCalculo()
+        {
+            OperadorFormulario operador = new OperadorFormulario(_listaTarjetas);
+            _txtCantPlasticoEmitidos.Text = operador.Cantidad.ToString();
+            _txtLimitePromedio.Text = operador.LimitePromedio.ToString();
+        }
+
+        private void RecargarLista()
+        {
+            _listaTarjetas = _servicioTarjeta.TraerTodas();
+            _lstTarjetas.DataSource = null;
+            _lstTarjetas.DataSource = _listaTarjetas;
+        }
+
+        private void ValidacionesCalculos()
         {
 
         }
